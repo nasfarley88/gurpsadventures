dbcbe7c27da9739669e4443943d3157f64d1f978
Nathanael Farley <nasfarley88@gmail.com>
2018-02-28 20:13:28 +0000

    Various changes to system51.

--- system51/Makefile
+++ system51/Makefile
@@ -1,8 +1,13 @@
+SHELL=/bin/bash
+
 all: main.pdf system51-player-guide.pdf
 
 %.pdf: *.tex
 	mkdir -p $(basename $@)_output
-	latexmk -interaction=nonstopmode -output-directory=$(basename $@)_output -lualatex -use-make $(basename $@).tex
+	ln -sf ${PWD}/images $(basename $@)_output/
+	ln -sf ${PWD}/*.tex $(basename $@)_output/
+	ln -sf ${PWD}/*.lua $(basename $@)_output/
+	latexmk -interaction=nonstopmode -lualatex -cd $(basename $@)_output/$(basename $@).tex
 
 clean:
 	latexmk -CA
--- system51/main.lua
+++ system51/main.lua
@@ -64,3 +64,80 @@ ia.getTotalPoints = function(innateAttack)
   return math.ceil(
     ia.getAdjustedPointsPerLevel(innateAttack)*innateAttack.level)
 end
+
+
+function totesseracont(n)
+  n_tesseracont = {}
+  if n <= 5 then
+    n_tesseracont[n] = true
+  elseif 5 < n  and n < 10 then
+    n_tesseracont[5] = true
+    n_tesseracont[n % 5] = true
+  elseif 10 <= n and n < 15 then
+    n_tesseracont[10] = true
+    n_tesseracont[n % 10] = true
+  elseif 15 <= n and n <= 20 then
+    n_tesseracont[15] = true
+    n_tesseracont[n % 15] = true
+  elseif 20 < n and n < 25 then
+    n_tesseracont[15] = true
+    n_tesseracont[5] = true
+    n_tesseracont[n % 20] = true
+  elseif 25 <= n and n <= 30 then
+    n_tesseracont[15] = true
+    n_tesseracont[10] = true
+    n_tesseracont[n % 25] = true
+  elseif 30 < n and n <= 34 then
+    n_tesseracont[15] = true
+    n_tesseracont[10] = true
+    n_tesseracont[5] = true
+    n_tesseracont[n % 30] = true
+  elseif 34 < n and n <= 37 then
+    n_tesseracont[15] = true
+    n_tesseracont[10] = true
+    n_tesseracont[5] = true
+    n_tesseracont[4] = true
+    n_tesseracont[n % 34] = true
+  elseif 37 < n and n <= 39 then
+    n_tesseracont[15] = true
+    n_tesseracont[10] = true
+    n_tesseracont[5] = true
+    n_tesseracont[4] = true
+    n_tesseracont[3] = true
+    n_tesseracont[n % 37] = true
+  elseif n == 40  then
+    n_tesseracont[15] = true
+    n_tesseracont[10] = true
+    n_tesseracont[5] = true
+    n_tesseracont[4] = true
+    n_tesseracont[3] = true
+    n_tesseracont[2] = true
+    n_tesseracont[1] = true
+  end
+
+  return n_tesseracont
+end
+
+function print_tesseracont(t)
+  if t[1] then
+    tex.sprint([[\one;]])
+  end
+  if t[2] then
+    tex.sprint([[\two;]])
+  end
+  if t[3] then
+    tex.sprint([[\three;]])
+  end
+  if t[4] then
+    tex.sprint([[\four;]])
+  end
+  if t[5] then
+    tex.sprint([[\five;]])
+  end
+  if t[10] then
+    tex.sprint([[\ten;]])
+  end
+  if t[15] then
+    tex.sprint([[\fifteen;]])
+  end
+end
